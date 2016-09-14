c879eb7f3fa317637c516100d07f26ee1a267a51
sudhatumuluru <sreesudha.tumuluru@gmail.com>
2016-09-14 15:12:58 -0700

    First Commit

---
+++ app/src/main/java/com/cmpe277/activitycounter/ActivityA.java
@@ -0,0 +1,13 @@
+package com.cmpe277.activitycounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a);
+    }
+}
---
+++ app/src/main/java/com/cmpe277/activitycounter/ActivityB.java
@@ -0,0 +1,7 @@
+package com.cmpe277.activitycounter;
+
+/**
+ * Created by sreesudha on 9/11/16.
+ */
+public class ActivityB {
+}

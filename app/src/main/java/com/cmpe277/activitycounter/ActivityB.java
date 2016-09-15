b8a31169120dde8ce52149ac13e90cfb3a93d900
sudhatumuluru <sreesudha.tumuluru@gmail.com>
2016-09-14 17:08:03 -0700

    XML files - added variables

--- app/src/main/java/com/cmpe277/activitycounter/ActivityB.java
+++ app/src/main/java/com/cmpe277/activitycounter/ActivityB.java
@@ -1,7 +1,16 @@
 package com.cmpe277.activitycounter;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by sreesudha on 9/11/16.
  */
-public class ActivityB {
+public class ActivityB extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
 }

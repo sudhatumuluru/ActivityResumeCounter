8f293a356bd9694e1c86dd60c157f145ede0d232
sudhatumuluru <sreesudha.tumuluru@gmail.com>
2016-09-15 22:58:21 -0700

    dialog activity added

--- app/src/main/java/com/cmpe277/activitycounter/ActivityA.java
+++ app/src/main/java/com/cmpe277/activitycounter/ActivityA.java
@@ -1,13 +1,40 @@
 package com.cmpe277.activitycounter;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class ActivityA extends AppCompatActivity {
+public class ActivityA extends Activity {
+
+    private String mActivityName;
+    private TextView mCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_a);
+        mActivityName = getString(R.string.activity_b_label);
+        mCounter = (TextView)findViewById(R.id.counter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    public void startDialog(View v){
+        Intent intent = new Intent(ActivityA.this,DialogActivity.class);
+        startActivity(intent);
+    }
+    public void startActivityB(View v){
+        Intent intent = new Intent(ActivityA.this,ActivityB.class);
+        startActivity(intent);
+    }
+    public void closeApp(View v){
+        ActivityA.this.finish();
     }
 }
---
+++ app/src/main/java/com/cmpe277/activitycounter/DialogActivity.java
@@ -0,0 +1,20 @@
+package com.cmpe277.activitycounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class DialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_dialog);
+    }
+
+    public void finishDialog(View v){
+        DialogActivity.this.finish();
+    }
+}

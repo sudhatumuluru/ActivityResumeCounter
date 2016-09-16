4f23e60e642c4394d300d0200761cc2df957864d
sudhatumuluru <sreesudha.tumuluru@gmail.com>
2016-09-15 23:10:04 -0700

    updated ActivityB added

--- app/src/main/java/com/cmpe277/activitycounter/ActivityB.java
+++ app/src/main/java/com/cmpe277/activitycounter/ActivityB.java
@@ -1,16 +1,18 @@
 package com.cmpe277.activitycounter;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 
-/**
- * Created by sreesudha on 9/11/16.
- */
-public class ActivityB extends AppCompatActivity{
+public class ActivityB extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b);
+    }
+
+    public void finishActivityB(View v){
+        ActivityB.this.finish();
     }
 }

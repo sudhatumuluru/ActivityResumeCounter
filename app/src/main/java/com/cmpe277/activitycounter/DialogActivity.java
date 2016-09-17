935a422464f7d30c2574a2c993612704a3cda322
sudhatumuluru <sreesudha.tumuluru@gmail.com>
2016-09-16 22:29:28 -0700

    added setFinishOnTouchOutside(false);

--- app/src/main/java/com/cmpe277/activitycounter/DialogActivity.java
+++ app/src/main/java/com/cmpe277/activitycounter/DialogActivity.java
@@ -12,6 +12,7 @@ public class DialogActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.setFinishOnTouchOutside(false);
         setContentView(R.layout.activity_dialog);
     }
 
